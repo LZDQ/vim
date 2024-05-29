91571e01805d3736569453a693670866e688e1fd
LZDQ <1486701401@qq.com>
2024-05-30 01:21:06 +0800

    Mix ctrlp, fzf and telescope. Use ;f for file, ;g for grep, etc

--- neovim/lua/config/jump.lua
+++ neovim/lua/config/jump.lua
@@ -20,6 +20,20 @@ require('telescope').setup{
 	}
 }
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', ';f', function() vim.cmd("Files") end, {})
+vim.keymap.set('n', ';F', builtin.find_files, {})
+vim.keymap.set('n', ';g', builtin.live_grep, {})
+vim.keymap.set('n', ';G', function() vim.cmd("Rg") end, {})
+vim.keymap.set('n', ';b', function() vim.cmd("Buffers") end, {})
+vim.keymap.set('n', ';B', builtin.buffers, {})
+vim.keymap.set('n', ';l', function() vim.cmd("Lines") end, {})
+vim.keymap.set('n', ';t', builtin.builtin, {})
+
+-- " fzf in CURRENT buffer
+-- nnoremap <silent>;l :Lines<CR>
+-- " fzf in files in this folder (use ctrlp instead)
+-- nnoremap <silent>;f :Files<CR>
+-- " fzf open buffer
+-- nnoremap <silent>;b :Buffers<CR>
+-- " fzf open ripgrep finder
+-- nnoremap <silent>;g :Rg<CR>
